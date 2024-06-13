a184563dba870986895904dd86eee3c28f5b610d
Zahar Litvinchuk <zahar@raynelz.ru>
2024-06-13 21:56:30 +0300

    Initial Commit

---
+++ EventifyApp/EventifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventifyAppApp.swift
+//  EventifyApp
+//
+//  Created by Захар Литвинчук on 13.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct EventifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

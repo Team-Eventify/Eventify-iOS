161020f2d65e431ea58448ed59c1be2a2e5d59e0
Gleb Rasskazov <rasskazov@1221systems.com>
2024-06-15 16:57:31 +0300

    eventify: [new] - сделан инпут

--- EventifyApp/Core/Auth/View/SignUpView.swift
+++ EventifyApp/Core/Auth/View/SignUpView.swift
@@ -8,52 +8,62 @@
 import SwiftUI
 
 struct SignUpView: View {
-	@State private var emailText: String = ""
-	@State  private var passwordText: String = ""
+    @State private var emailText: String = ""
+    @State private var passwordText: String = ""
 
     var body: some View {
-		ZStack {
-			Color.background.ignoresSafeArea()
+        ZStack {
+            Color.background.ignoresSafeArea()
 
-			VStack(alignment: .leading) {
-				VStack(alignment: .leading, spacing: 0) {
-					Text("Регистрация")
-						.font(.custom(Fonts.semibold, size: 40))
-						.foregroundStyle(Color.mainText)
+            VStack(alignment: .leading, spacing: 60) {
+                registrationContentContainerView
+                registrationButtonContainerView
+            }
+            .foregroundStyle(Color.secondaryText)
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal, 16)
+        }
+    }
+    
+    private var registrationContentContainerView: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text("Регистрация")
+                .font(.semiboldCompact(size: 40))
+                .foregroundStyle(Color.mainText)
 
-					Text("Пожалуйста, создайте новый аккаунт.\nЭто займёт меньше минуты.")
-						.font(.custom(Fonts.regular, size: 17))
-						.foregroundStyle(Color.secondaryText)
-						.padding(.top, 12)
+            Text("Пожалуйста, создайте новый аккаунт. Это займёт меньше минуты.")
+                .font(.regularCompact(size: 17))
+                .frame(width: 296)
+        }
+    }
+    
+    private var registrationButtonContainerView: some View {
+        VStack(spacing: 20) {
+            EventifyButton(title: "Зарегистрироваться") {
+                print("Tapped")
+            }
 
-				}
-				EventifyButton(title: "Зарегистрироваться") {
-					print("Tapped")
-				}
-				.padding(.top, 60)
+            haveAccountContainerView
+        }
+    }
 
-				HStack(spacing: 12) {
-					Text("Уже есть аккаунт?")
-						.font(.custom(Fonts.regular, size: 16))
-						.foregroundStyle(Color.secondaryText)
-					Button {
-						print("Войти tapped!")
-					} label: {
-						Text("Войти")
-							.underline()
-							.font(.custom(Fonts.medium, size: 16))
-							.foregroundStyle(Color.brandYellow)
-					}
-				}
-				.frame(maxWidth: .infinity)
-				.padding(.top, 20)
-			}
-			.frame(maxWidth: .infinity)
-			.padding(.horizontal, 16)
-		}
+    private var haveAccountContainerView: some View {
+        HStack(spacing: 12) {
+            Text("Уже есть аккаунт?")
+                .font(.regularCompact(size: 16))
+            Button {
+                print("Войти tapped!")
+            } label: {
+                Text("Войти")
+                    .underline()
+                    .font(.mediumCompact(size: 16))
+                    .foregroundStyle(Color.brandYellow)
+            }
+        }
+        .frame(maxWidth: .infinity)
     }
 }
 
 #Preview {
-	SignUpView()
+    SignUpView()
 }

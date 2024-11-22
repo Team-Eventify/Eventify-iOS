42b61576f576e18c8647a988ee5a0152d6eb2e4a
Zahar Litvinchuk <zahar@raynelz.ru>
2024-11-22 15:42:37 +0300

    [EVE-45]: Инициализиция Fluent базу данных

---
+++ EventifyAppTests/EventifyAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  EventifyAppTests.swift
+//  EventifyAppTests
+//
+//  Created by Захар Литвинчук on 21.11.2024.
+//
+
+import Testing
+
+struct EventifyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
